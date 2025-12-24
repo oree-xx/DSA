12fe12fdf07d03695a07d9fc8f26a04e36069051
Oreoluwa Oluwasina <oreoluwaoluwasina@gmail.com>
2025-12-24 19:43:09 +0100

    patterns

---
+++ Patterns.py
@@ -0,0 +1,46 @@
+def pattern1(N):
+    for i in range(N):
+        for j in range(N):
+            print("*", end=" ")
+        print()
+
+#pattern1(3)
+#pattern1(5)
+
+def pattern2(N):
+    for i in range(N):
+        print("*" * (i + 1))
+
+#pattern2(3)
+
+def pattern3(N):
+    for i in range(1, N + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+#pattern3(5)
+
+def pattern4(N):
+    for i in range(1, N + 1):
+        for j in range(1, i+1):
+            print(i, end=" ")
+        print()
+
+#pattern4(5)
+
+def pattern5(N):
+    for i in range(N):
+        for j in range(N, i, -1):
+            print("*", end=" ")
+        print()
+        
+#pattern5(5)
+
+def pattern6(N):
+    for i in range(N):
+        for j in range(N, i, -1):
+            print(N-j+1, end=" ")
+        print()
+
+#pattern6(5)
